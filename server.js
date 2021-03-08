e41107ed36898d6e08f02189bb0bc75736f65229
Bryan Moreno <bfourbryan@gmail.com>
2021-03-08 15:32:25 -0800

    Work on server.js: Required fs, path, and moment. Required moment to add a timestamp to added notes to use to later delete. Addition of app.get and app.post as well as routes for /notes and *

--- server.js
+++ server.js
@@ -1,48 +1,85 @@
 var express = require("express");
 //require the fs module to import it.
+var fs = require("fs");
+var path = require("path");
+// Required Moment to add a `timeStamp` when a pst requires and id
+var moment = require("moment");
+var timeStamp = moment().format("x");
+
 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+function init() {
+    console.log(timeStamp);
+};
 
-app.get("/api/notes", function(req, res) {
+init();
 
-    // Use the fs module to read the file
-
-    // THEN parse the file contents with json.parse() to get the real data
-
-    // send the parsed data back to the client with res.json()
-});
-
-app.post("/api/notes", function(req, res) {
-
-    // Access the POSTed data in `req.body`
+app.get("/api/notes", function (req, res) {
 
     // Use the fs module to read the file
+    fs.readFile(path.join(__dirname + "/db/db.json"), function (err, data) {
+        if (err) {
+            throw err;
+        };
 
-    // THEN parse the file contents with json.parse() to get the real data
+        // THEN parse the file contents with json.parse() to get the real data
+        var infoRead = JSON.parse(data);
 
-    // PUSH the `req.body` to the array list
+        // send the parsed data back to the client with res.json()
+        res.json(infoRead);
 
-    // JSON.stringify() the array list back into a JSON string
+    });
+});
 
-    // THEN save the contents back to the `db.json` with the fs module
+app.post("/api/notes", function (req, res) {
+
+    // Access the POSTed data in `req.body` add an id to post
+    var addNewNote = req.body
+    addNewNote.id = timeStamp();
+
+    // Use the fs module to read the file
+    fs.readFile(path.join(__dirname + "/db/db.json"), function (err, data) {
+        if (err) {
+            throw err;
+        };
+        
+        // THEN parse the file contents with json.parse() to get the real data
+        var notesAdded = JSON.parse(data);
+                
+        // PUSH the `req.body` to the array list
+        notesAdded.push(addNewNote);
+
+        // JSON.stringify() the array list back into a JSON string
+        var stringifyNotesAdded = JSON.stringify(notesAdded);
+
+        // THEN save the contents back to the `db.json` with the fs module
+        fs.writeFile(path.join(__dirname + "/db/db.json"), stringifyNotesAdded, function (err) {
+            if (err) {
+                throw err;
+            };
+            res.json(notesAdded);
+        });
+
+    });
 
 });
 
-app.delete("/api/notes/:id", function(req, res) {
+app.delete("/api/notes/:id", function (req, res) {
 
     // Access the :id from the `req.params.id`
-    
+
     // Use the fs module to read the file
 
     // THEN parse the file contents with json.parse() to get the real data
-    
+
     // Option A
     // Find the matching index using .findIndex()
     // Remove the target element using .splice()
@@ -52,23 +89,23 @@ app.delete("/api/notes/:id", function(req, res) {
     // myarray = myarray.filter( element => element.id !== req.params.id );
 
     // Return a success message.
-    
+
 })
 
-app.get("/notes", function(req, res) {
-    
+app.get("/notes", function (req, res) {
+
     //Return the contents of the notes.html
-    res.sendFile( /** path to the notes.html */)
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 
 });
 
-app.get("*", function(req, res) {
-    
+app.get("*", function (req, res) {
+
     //Return the contents of the index.html
-    res.sendFile( /** path to the index.html */)
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
